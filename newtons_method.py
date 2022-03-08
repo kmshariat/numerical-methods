54ef376052485f473af3cd7f4f0a12ddf22ff9ec
K.M Shariat Ullah <cast.shariat@gmail.com>
2022-03-08 20:27:07 +0600

    initial guess, condition and display
    Updated user input to get initial guess and added if-else condition for the undefined region. added some text to print the roots after some iteration.

--- newtons_method.py
+++ newtons_method.py
@@ -1,9 +1,20 @@
 #Newton Raphson Method
+
+#this function takes the function
 def f(x):
   return x**3 - 3*x**2 +5*x - 7
+
+#this function takes the derivative
 def df(x):
   return 3*x**2 - 6*x + 5 
-x = 2
-for i in range(10):
-  x = x - f(x)/df(x)
-  print(x)
+
+#user input for initial guess
+x = float(input("Enter your initial guess: "))
+
+#if df(x)==0 then the denominator in N-R method becomes zero hence the solution becomes undefined
+if df(x) == 0:
+  print("Solution is not possible using N-R Method")
+else:
+  for i in range(10):
+    x = x - f(x)/df(x)
+    print(f"After {i}th iteration the root is: {x}")
